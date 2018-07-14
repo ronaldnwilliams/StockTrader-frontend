3fa4b8de3c6d9655acce7d0dd63fe4427fc12235
Ronald Williams <ronaldnwilliams@gmail.com>
2018-07-14 12:54:35 -0500

    updated spacing on indices bar

--- src/components/IndicesBar.js
+++ src/components/IndicesBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { percentFormat } from '../AppMethods';
+import { currencyFormat, percentFormat } from '../AppMethods';
 
 
 class IndicesBar extends Component {
@@ -41,8 +41,13 @@ class IndicesBar extends Component {
       return (
         <div className='row w-100 mx-auto mb-2 p-2 bg-dark border border-white rounded'>
           {indices.map(index => (
-            <div className='col-lg p-1' key={index.label}>
-              {index.quote.symbol} {index.quote.latestPrice}
+            <div className='col-lg text-center p-1' key={index.label}>
+              <span className='mx-1'>
+                {index.quote.symbol}
+              </span>
+              <span className='mx-1'>
+                {currencyFormat(index.quote.latestPrice)}
+              </span>
               <span className={Number(index.quote.changePercent) > 0 ? 'text-success' : 'text-danger'}>
                 {percentFormat(index.quote.changePercent)}
               </span>
